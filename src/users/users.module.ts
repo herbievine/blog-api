c93b030cc2365618f698334eeec2d3f6cb71d1f3
Herbie Vine <vineherbie@gmail.com>
2021-11-04 21:07:49 +0100

    updated readme

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { UsersService } from './users.service'
 import { AuthModule } from 'src/auth/auth.module'
+import { UsersResolver } from './users.resolver';
 
 @Module({
   imports: [AuthModule],
-  providers: [PrismaService, UsersService],
+  providers: [PrismaService, UsersService, UsersResolver],
   exports: [UsersService]
 })
 export class UsersModule {}
